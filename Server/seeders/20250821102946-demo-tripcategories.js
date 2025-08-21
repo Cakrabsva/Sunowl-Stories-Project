1742a29ae3f5dbca7459bc1e99f5096918c43b36
Cakrabsva <cakrabilisairo.va@gmail.com>
2025-08-21 18:21:10 +0700

    Seeding TripCategories Data

--- Server/seeders/20250821102946-demo-tripcategories.js
+++ Server/seeders/20250821102946-demo-tripcategories.js
@@ -6,25 +6,21 @@ module.exports = {
     await queryInterface.bulkInsert('TripCategories',
       [{
         "id": 1,
-        "OpenTripId": 2,
         "category_name": "Montain",
         "createdAt":new Date (),
         "updatedAt":new Date ()
       }, {
         "id": 2,
-        "OpenTripId": 3,
         "category_name": "Island",
         "createdAt":new Date (),
         "updatedAt":new Date ()
       }, {
         "id": 3,
-        "OpenTripId": 4,
         "category_name": "Camping",
         "createdAt":new Date (),
         "updatedAt": new Date ()
       }, {
         "id": 4,
-        "OpenTripId": 5,
         "category_name": "Museum",
         "createdAt":new Date (),
         "updatedAt":new Date ()
